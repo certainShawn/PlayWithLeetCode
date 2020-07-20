5e1fcd06c782f2aade1445b4a495c6853a7a226a
A_certain_Shawn <alex.y0203@gmail.com>
2020-07-20 18:20:01 +0900

    Add files via upload

---
+++ LeetCode/TestClass.java
@@ -0,0 +1,16 @@
+public class TestClass {
+    public static void main(String args[])
+    {
+        LeetCode13 leetCode13=new LeetCode13();
+        int testcase2=leetCode13.romanToInt("III");
+        int testcase3=leetCode13.romanToInt("IV");
+        int testcase4=leetCode13.romanToInt("IX");
+        int testcase5=leetCode13.romanToInt("LVIII");
+        int testcase6=leetCode13.romanToInt("MCMXCIV");
+        System.out.print(testcase2);
+        System.out.print(testcase3);
+        System.out.print(testcase4);
+        System.out.print(testcase5);
+        System.out.print(testcase6);
+    }
+}
