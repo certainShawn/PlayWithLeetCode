1f069aadc71fea39da6af18a08d7539ca9e0799f
A_certain_Shawn <alex.y0203@gmail.com>
2020-11-04 14:49:26 +0900

    Add files via upload

--- LeetCode/Solution.java
+++ LeetCode/Solution.java
@@ -1,23 +1,21 @@
-import java.lang.*;
-import java.util.Arrays;
 class Solution {
     
     public int reverse(int x) {     
         Integer miden=new Integer(x);
         String numString=miden.toString();
+        StringBuilder stringBuilder=new StringBuilder(numString);
+        if(stringBuilder.charAt(0)=='-')
+        {
+            stringBuilder.deleteCharAt(0);
+        }
         char[] charater=new char[numString.length()-1];
-        for(int i=0;i<numString.length();i++)
+        for(int i=0;i<stringBuilder.length();i++)
         {
-            StringBuilder stringBuilder=new StringBuilder(numString);
-            
-            charater[i]=stringBuilder.charAt(numString.length()-i);
+            charater[stringBuilder.length()-1-i]=stringBuilder.charAt(stringBuilder.length()-i);
         }
-        if(charater[(numString.length()-1)]=='-')
-        {
-            char[] charater1=Arrays.copyOfRange(charater,0, numString.length()-2)
-        }
-        int result=0;
+        String resultmiddien=charater.toString();
+        Integer resultfinal=new Integer(resultmiddien);
+        int result=resultfinal.intValue();
     return result;
     }
-
 }
